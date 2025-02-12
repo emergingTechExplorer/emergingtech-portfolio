1aabd3fc0699d0949ecca7e17f76e2b2d63697e1
Lakshantha Dissanayake <lakshanthad@gmail.com>
2025-02-12 01:34:47 -0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { Provider } from "@/components/ui/provider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
